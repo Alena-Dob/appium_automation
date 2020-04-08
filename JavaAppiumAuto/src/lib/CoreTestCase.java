05d6011c2ccec2794469bcec7cdf917b6ee06c2c
Alena-Dob <Lesyonok.s@mail.ru>
2020-04-09 02:47:21 +0300

    Ex 10

--- JavaAppiumAuto/src/lib/CoreTestCase.java
+++ JavaAppiumAuto/src/lib/CoreTestCase.java
@@ -7,8 +7,12 @@ import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
+import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
+
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
 
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
@@ -17,16 +21,8 @@ public class CoreTestCase extends TestCase {
     protected void setUp() throws Exception
     {
         super.setUp();
-        DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "6.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/Users/ddv/Desktop/JavaAppiumAuto/apks/org.wikipedia.apk");
-
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
         driver = new AndroidDriver(new URL(AppiumURL), capabilities);
         this.rotateScreenPortrait();
     }
@@ -51,6 +47,30 @@ public class CoreTestCase extends TestCase {
 
     protected void backgroundApp(int seconds)
     {
-        driver.runAppInBackground(seconds);
+        driver.runAppInBackground(Duration.ofSeconds(seconds));
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+
+        String platform = System.getenv("PLATFORM");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "AndroidTestDevice");
+            capabilities.setCapability("platformVersion", "6.0");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "/Users/ddv/Documents/GitHub/appium_automation/JavaAppiumAuto/apks/org.wikipedia.apk");
+        } else if (platform.equals(PLATFORM_IOS)) {
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("deviceName", "iPhone 11 Pro Max");
+            capabilities.setCapability("platformVersion", "13.4");
+            capabilities.setCapability("app", "/Users/ddv/Documents/GitHub/appium_automation/JavaAppiumAuto/apks/Wikipedia.app");
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+        return capabilities;
     }
 }
--- JavaAppiumAuto/src/lib/ui/SearchPageObject.java
+++ JavaAppiumAuto/src/lib/ui/SearchPageObject.java
@@ -9,15 +9,15 @@ import java.util.List;
 public class SearchPageObject extends MainPageObject
 {
     private static final String
-    SEARCH_INIT_ELEMENT = "//*[contains(@text,'Search Wikipedia')]",
-    SEARCH_INPUT = "//*[contains(@text,'Search…')]",
-    SEARCH_CANCEL_BUTTON = "org.wikipedia:id/search_close_btn",
-    SEARCH_RESULT_BY_SUBSTRING_TPL = "//*[@resource-id='org.wikipedia:id/search_results_container']//*[@text='{SUBSTRING}']",
-    SEARCH_RESULT_ELEMENT = "//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']",
-    SEARCH_EMPTY_RESULT_ELEMENT = "//*[@text='No results found']",
-    SEARCH_INPUT_DEFAULT_TEXT = "org.wikipedia:id/search_src_text",
-    SEARCH_RESULT_LIST = "//*[@resource-id='org.wikipedia:id/search_results_list']",
-    SEARCH_RESULTS_LIST_TITLE = "org.wikipedia:id/page_list_item_title";
+    SEARCH_INIT_ELEMENT = "xpath://*[contains(@text,'Search Wikipedia')]",
+    SEARCH_INPUT = "xpath://*[contains(@text,'Search…')]",
+    SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn",
+    SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://*[@resource-id='org.wikipedia:id/search_results_container']//*[@text='{SUBSTRING}']",
+    SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']",
+    SEARCH_EMPTY_RESULT_ELEMENT = "xpath://*[@text='No results found']",
+    SEARCH_INPUT_DEFAULT_TEXT = "id:org.wikipedia:id/search_src_text",
+    SEARCH_RESULT_LIST = "xpath://*[@resource-id='org.wikipedia:id/search_results_list']",
+    SEARCH_RESULTS_LIST_TITLE = "id:org.wikipedia:id/page_list_item_title";
 
     public SearchPageObject(AppiumDriver driver)
     {
@@ -29,87 +29,88 @@ public class SearchPageObject extends MainPageObject
     {
         return SEARCH_RESULT_BY_SUBSTRING_TPL.replace("{SUBSTRING}", substring);
     }
+
     /* TEMPLATES METHODS */
 
     public void initSearchInput()
     {
-        this.waitForElementAndClick(By.xpath(SEARCH_INIT_ELEMENT), "Cannot find and click search element", 5);
-        this.waitForElementPresent(By.xpath(SEARCH_INIT_ELEMENT), "Cannot find search input after clicking search init element");
+        this.waitForElementAndClick(SEARCH_INIT_ELEMENT, "Cannot find and click search element", 5);
+        this.waitForElementPresent(SEARCH_INIT_ELEMENT, "Cannot find search input after clicking search init element");
     }
 
     public void waitForCancelButtonToAppear()
     {
-        this.waitForElementPresent(By.id(SEARCH_CANCEL_BUTTON), "Cannot find search cancel button!", 5);
+        this.waitForElementPresent(SEARCH_CANCEL_BUTTON, "Cannot find search cancel button!", 5);
     }
 
     public void waitForCancelButtonToDisappear()
     {
-        this.waitForElementNotPresent(By.id(SEARCH_CANCEL_BUTTON), "Search cancel button is still present!", 5);
+        this.waitForElementNotPresent(SEARCH_CANCEL_BUTTON, "Search cancel button is still present!", 5);
     }
 
     public void clickCancelSearch()
     {
-        this.waitForElementAndClick(By.id(SEARCH_CANCEL_BUTTON), "Cannot find and click search cancel button", 5);
+        this.waitForElementAndClick(SEARCH_CANCEL_BUTTON, "Cannot find and click search cancel button", 5);
     }
 
     public void typeSearchLine(String search_line)
     {
-        this.waitForElementAndSendKeys(By.xpath(SEARCH_INPUT), search_line, "Cannot find and type into search input", 5);
+        this.waitForElementAndSendKeys(SEARCH_INPUT, search_line, "Cannot find and type into search input", 5);
     }
 
     public void waitForSearchResult(String substring)
     {
         String search_result_xpath = getResultSearchElement(substring);
-        this.waitForElementPresent(By.xpath(search_result_xpath), "Cannot find search result with substring " + substring);
+        this.waitForElementPresent(search_result_xpath, "Cannot find search result with substring " + substring);
     }
 
     public void waitForSearchListToAppear(String search_line)
     {
-        this.waitForElementPresent(By.xpath(SEARCH_RESULT_LIST), "Cannot find results for " + search_line, 5);
+        this.waitForElementPresent(SEARCH_RESULT_LIST, "Cannot find results for " + search_line, 5);
     }
 
     public void waitForSearchListToDisappear()
     {
-        this.waitForElementNotPresent(By.xpath(SEARCH_RESULT_LIST), "Results still present on the page", 5);
+        this.waitForElementNotPresent(SEARCH_RESULT_LIST, "Results still present on the page", 5);
     }
 
     public void clickByArticleWithSubstring(String substring)
     {
         String search_result_xpath = getResultSearchElement(substring);
-        this.waitForElementAndClick(By.xpath(search_result_xpath), "Cannot find search result with substring " + substring, 10);
+        this.waitForElementAndClick(search_result_xpath, "Cannot find search result with substring " + substring, 10);
     }
 
     public int getAmountOfFoundArticles()
     {
         this.waitForElementPresent(
-                By.xpath(SEARCH_RESULT_ELEMENT),
+                SEARCH_RESULT_ELEMENT,
                 "Cannot find anything by the request",
                 15
         );
 
-        return this.getAmountOfElements(By.xpath(SEARCH_RESULT_ELEMENT));
+        return this.getAmountOfElements(SEARCH_RESULT_ELEMENT);
     }
 
     public void waitForEmptyResultsLabel()
     {
-        this.waitForElementPresent(By.xpath(SEARCH_EMPTY_RESULT_ELEMENT), "Cannot find empty results ELEMENT", 15);
+        this.waitForElementPresent(SEARCH_EMPTY_RESULT_ELEMENT, "Cannot find empty results ELEMENT", 15);
     }
 
     public void assertThereIsNoResultOfSearch()
     {
-        this.assertElementNotPresent(By.xpath(SEARCH_RESULT_ELEMENT), "We supposed not to find any results elements");
+        this.assertElementNotPresent(SEARCH_RESULT_ELEMENT, "We supposed not to find any results elements");
     }
 
     public String getSearchInputDefaultText()
     {
-        WebElement text_element = this.waitForElementPresent(By.id(SEARCH_INPUT_DEFAULT_TEXT), "Cannot find search input after clicking search init element");
+        WebElement text_element = this.waitForElementPresent(SEARCH_INPUT_DEFAULT_TEXT, "Cannot find search input after clicking search init element");
         return text_element.getAttribute("text");
     }
 
     public void checkContainSearchElementInResultsList(String search_attribute, String search_line)
     {
         this.waitForElementContains(
-                By.id(SEARCH_RESULTS_LIST_TITLE),
+                SEARCH_RESULTS_LIST_TITLE,
                 search_attribute,
                 search_line,
                 "Cannot contain search line: " + search_line,
